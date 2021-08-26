549f77745055d88f02c1253eba5fd9efef73fd15
ethan <grokMonkey@gmail.com>
2021-08-25 20:29:18 -0400

    saving work up to page 28

---
+++ src/alloc.c
@@ -0,0 +1,88 @@
+/* exported macros */
+
+#define NEW(p,a) ((p) = allocate(sizeof *(p), (a)))
+#define NEW0(p,a) memset(NEW((p),(a)), 0, sizeof *(p))
+
+/* typedefs */
+#include "config.h"
+#include "c.h"
+/* interface 78 */
+/* exported types */
+/* exported data */
+/* exported functions */
+
+// n = length, a = identifier
+extern void *allocate ARGS((unsigned long n, unsigned a));
+extern void deallocate ARGS((unsigned a));
+
+extern void *newarray
+	ARGS((unsigned long m, unsigned long n, unsigned a));
+
+/* types */
+
+struct block {
+	struct block *next;
+	char *limit;
+	char *avail;
+};
+
+union align {
+	long l;
+	char *p;
+	double d;
+	int (*f) ARGS((void));
+};
+
+union header {
+	struct block b;
+	union align a;
+};
+
+
+#ifef PURIFY
+/* debugging implementation */
+#else
+/* data */
+
+static struct block
+	first[] = { { NULL }, { NULL }, { NULL } },
+	*arena[] = { &first[0], &first[1], &first[2] };
+
+static struct block *freeblocks;
+
+/* functions */
+
+void *allocate(n, a) unsigned long n; unsigned a; {
+	struct block *ap;
+
+	ap = arena[a];
+	n = roundup(n, sizeof (union align));
+	while (ap->avail + n > ap->limit) {
+		/* get a new block */
+		if ((ap->next = freeblocks) != NULL) {
+			freeblocks = freeblocks->next;
+		} else {
+			/* allocate a new block */
+			unsinged m = sizeof(union header) + n + 10*1024;
+			ap->next = malloc(m);
+			ap = ap->next;
+			if (ap == NULL) {
+				error("insufficient memory\n");
+				exit(1);
+			}
+			ap->limit = (char *)ap + m;
+		}
+		ap->avail = (char *)((union header *)ap + 1);
+		ap->next = NULL;
+		arena[a] = ap;
+	}
+
+	ap->avail += n;
+	return ->avail - n;
+}
+
+void * newarray(m, n, a) unsigned long m, n; unsigned a; {
+	return allocate(m*n, a);
+}
+
+#endif
---
+++ src/c.h
@@ -0,0 +1,36 @@
+/* exported macros */
+
+#ifdef __STDC__
+#define ARGS(list) list
+#else
+#define ARGS(list) ()
+#endif
+
+#ifdef __STDC__
+include <stdarg.h>
+#define va_init(a,b) va_start(a,b)
+#else
+#include <varargs.h>
+#define va_init(a,b) va_start(a)
+#endif
+
+#ifdef __STDC__
+#define VARARGS(newlist,oldlist,olddcls) newlist
+#else
+#define VARARGS(newlist,oldlist,olddcls) oldlist olddcls
+#endif
+
+/* supports pointers of different sizes than int/word */
+#define NULL ((void*)0)
+
+#define NELEMS(a) ((int)(sizeof (a)/sizeof ((a)[0])))
+#define roundup(x,n) (((x)+((n)-1))&(~((n)-1)))
+
+<typedefs>
+#include "config.h"
+<interface 78>
+<exported types>
+<exported data>
+<exported functions>
+
+
