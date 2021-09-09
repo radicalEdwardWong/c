4ee92603c7234ba9c3520d42b4a8f5a8e8d098d7
ethan <grokmonkey@gmail.com>
2021-09-08 22:25:53 -0400

    adding 3.2 stuff

--- src/sym.c
+++ src/sym.c
@@ -30,6 +30,14 @@ struct symbol {
 enum { CONSTANTS =1, LABELS, GLOBAL, PARAM, LOCAL };
 
 /* exported data */
+
+extern Table constants;
+extern Table externals;
+extern Table global;
+extern Table identifiers;
+extern Table labels;
+extern Table types;
+
 /* exported functions */
 
 /* macros */
@@ -41,8 +49,60 @@ typdef struct coord {
 	unsigned x, y;
 } Coordinate;
 
+typedef struct table *Table;
+
 /* types */
+
+struct table {
+	int level;
+	Table previous;
+	struct entry {
+		symbol sym;
+		struct entry *link;
+	} *buckets[256];
+	Symbol all;
+};
+#define HASHSIZE NELEMS(((Table)0)->buckets)
+
 /* data */
+
+static struct table
+	cns = { CONSTANTS },
+	ext = { GLOBAL },
+	ids = { GLOBAL },
+	tys = { GLOBAL };
+Table constants = &cns;
+Table externals = &ext;
+Table identifiers = &ids;
+Table globals = &ids;
+Table types = &tys;
+Table labels;
+
 /* functions */
 
+Table table(tp, level) Tble tp; int level; {
+	Table new;
 
+	NEW0(new, FUNC);
+	new->previous = tp;
+	new->level = level;
+	if (tp)
+		new->all = tp->all;
+	return new;
+}
+
+void foreach(tp, lev, apply, cl) Table tp; int lev;
+void (*apply) ARGS((Symbol, void *)); void *cl {
+	while (tp && tp->level > lev)
+		tp = tp->previous;
+	if (tp && tp->level == lev) {
+		Symbol p;
+		Coordinate sav;
+		sav = src;
+		for (p = tp->all; p && p->scope == lev; p = p->up) {
+			src = p->sc;
+			(*apply)(p, cl);
+		}
+		src = sav;
+	}
+}
