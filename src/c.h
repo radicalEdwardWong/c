1a77c7fefc62a1e3492ecc12698a9b2c8c340e87
Ethan <grokMonkey@gmail.com>
2021-12-04 01:17:51 -0500

    adding ch 1

--- src/c.h
+++ src/c.h
@@ -1,4 +1,29 @@
 <exported macros>
+#ifdef __STDC__
+#define ARGS(list) list
+#else
+#define ARGS(list) ()
+#endif
+
+#ifdef __STDC__
+#include <stdarg.h>
+#define va_init(a,b) va_start(a,b)
+#else
+#include <varargs.h>
+#define va_init(a,b) va_start(a)
+#endif
+
+#ifdef __STDC__
+#define VARARGS(newlist,oldlist,oldcls) newlist
+#else
+#define varargs(newlist,oldlist,oldcls) oldlist oldcls
+#endif
+
+#define NULL ((void*)0)
+
+#define NELEMS(A) ((int)(sizeof (a)/sizeof ((a)[0})))
+#define roundup(x,n) (((x)+((n)-1)&(~((n)-1)))
+
 <typedefs>
 #include "config.h"
 <interface 78>
--- src/module.c
+++ src/module.c
@@ -1,6 +1,6 @@
 #include "c.h"
-<M macros>
-<M types>
-<M prototypes>
-<M data>
-<M functions>
+<macros>
+<types>
+<prototypes>
+<data>
+<functions>
