f505142c70f3f2f1e1976c306dd4d3c5f7768050
Ethan <grokMonkey@gmail.com>
2021-12-07 23:46:34 -0500

    adding chp 3 code

--- src/sym.c
+++ src/sym.c
@@ -1,5 +1,8 @@
 #include "c.h"
 /* macros */
+
+#define equalp(x) v.x == p->sym.u.c.v.x
+
 /* typedefs */
 
 typedef struct symbol *Symbol;
@@ -11,6 +14,18 @@ typedef struct coord {
 
 typedef struct table *Table;
 
+typedef union value {
+	/* signed */ char sc;
+	short ss;
+	int i;
+	unsigned char uc;
+	unsigned short us;
+	unsigned int u;
+	float f;
+	double d;
+	void *p;
+} Value;
+
 /* exported types */
 
 struct symbol {
@@ -20,15 +35,26 @@ struct symbol {
 	Symbol up;
 	List uses;a
 	int sclass;
-	/* symbol flags 50 */
+	/* symbol flags */
+	unsigned temporary:1;
+	unsigned generated:1;
+	unsigned defined:1;
 	Type type;
 	float ref;
 	union {
-		/* labels 46 */
+		/* labels */
+		struct {
+			int label;
+			Symbol equatedto;
+		} l;
 		/* struct types 65 */
 		/* enum constants 69 */
 		/* enum types 68 */
-		/* constants 47 */
+		/* constants */
+		struct {
+			Value v;
+			Symbol loc;
+		} c;
 		/* function symbols 290 */
 		/* globals 265 */
 		/* temporaries 346 */
@@ -60,6 +86,8 @@ extern Table identifiers;
 extern Table labels;
 extern Table types;
 
+extern List loci, symbols;
+
 /* data */
 
 static struct table
@@ -79,6 +107,12 @@ Coordinate src;
 extern int level;
 
 int level = GLOBAL;
+
+/* exported functions */
+
+extern void use ARGS((Symbol p, Coordinate src));
+
+extern void locus ARGS((Table tp, Coordinate *cp));
 
 /* functions */
 
@@ -156,4 +190,98 @@ Symbol lookup(name, tp) char *name; Table tp; {
 	while ((tp = tp->previous) != NULL);
 
 	return NULL;
+}
+
+int genlabel(n) int n; {
+	static int label = 1;
+
+	label += n;
+	return label - n;
+}
+
+Symbol findlabel(lab) int lab; {
+	struct entry *p;
+	unsigned h = lab&(HASHSIZE-1);
+
+	for (p = labels->buckets[h]; p; p = p->link)
+		if (lab == p->sym.u.l.label)
+			return &p->sym;
+
+	NEW0(p, FUNC);
+	p->sym.name = stringd(lab);
+	p->sym.scope = LABELS;
+	p->sym.up = labels->all;
+	labels->all = &p->sym;
+	p->link = labels->buckets[h];
+	labels->buckets[h] = p;
+	p->sym.generated = 1;
+	p->sym.u.l.label = lab;
+	(*IR->defsymbol)(&p->sym);
+	return &p->sym;
+}
+
+Symbol constants(ty, v) Type ty, Value v; {
+	struct entry *p;
+	unsigned h = v.u&(HASHSIZE-1);
+
+	ty = unqual(ty);
+	for (p = constants->buckets[h]; p; p = p->link)
+		if (eqtype(ty, p->sym.type, 1))
+			/* return the symbol if p's value == v */
+			switch (ty->op) {
+				case CHAR:		if (equalp(uc))	return &p->sym; break;
+				case SHORT:		if (equalp(ss))	return &p->sym; break;
+				case INT:		if (equalp(i))	return &p->sym; break;
+				case UNSIGNED:	if (equalp(u))	return &p->sym; break;
+				case FLOAT:		if (equalp(f))	return &p->sym; break;
+				case DOUBLE:	if (equalp(d))	return &p->sym; break;
+				case ARRAY: case FUNCTION:
+				case POINTER:	if (equalp(p)
+			}
+
+	NEW0(p, PERM);
+	p->sym.name = vtoa(ty, v);
+	p->sym.scope = CONSTANTS;
+	p->sym.type = ty;
+	p->sym.sclass = STATIC;
+	p->sym.u.c.v = v;
+	p->link = constants->buckets[h];
+	p->sym.up = constants->all;
+	constants->all = &p->sym;
+	constants->buckets[h] = p;
+	/* announce the constant, if necessary */
+	if (ty->u.sym && !ty->u.sym->addressed)
+		(*IR->defsymbol)(&p->sym);
+
+	p->sym.defined = 1;
+	return &p->sym;
+}
+
+Symbol genident(scls, ty, lev) int scls, lev; Type ty; {
+	Symbol p;
+
+	NEW0(p, lev >= LOCAL ? FUNC : PERM);
+	p->name = stringd(genlabel(1));
+	p->scope = lev;
+	p->sclass = scls;
+	p->type = ty;
+	p->generated = 1;
+	if (lev == GLOBAL)
+		(*IR->defsymbol)(p);
+	return p;
+}
+
+Symbol temporary(scls, ty, lev) Type ty; int scls, lev; {
+	Symbol p = genident(scls, ty, lev);
+
+	p->temporary = 1;
+	return p;
+}
+
+Symbol newtemp(sclass, tc) int sclass, tc; {
+	Symbol p = temporary(sclass, btot(tc), LOCAL);
+
+	(*IR->local)(p);
+	p->defined = 1;
+	return p;
 }
